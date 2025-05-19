2b412247c1da77d6b1aaab64161f7d387df600cd
Daniela Macias <dmacias.ufl.edu>
2025-05-19 11:17:04 -0400

    Designing background and first message

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import sandpaper from "./components/sandpaper.png";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        backgroundColor:  '#e9d7c0',
+        backgroundImage: `url(${sandpaper})`,
+        backgroundSize: "auto",
+        backgroundRepeat: 'repeat',
+        backgroundPosition: "center",
+        height: '100vh',
+        width: '100vw',
+        margin: 0
+      }}
+    >
+      <h1>Welcome to My Portfolio</h1>
+    </div>
+  );
 }
 
 export default App
